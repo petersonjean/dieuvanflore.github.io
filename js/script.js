ef95492908e7ebc482325a05fccbcd887d56356c
petersonjean45 <petersonjean45@gmail.com>
2017-06-27 18:43:04 -0400

    .
    .

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ $('#input-form').one('submit',function(){
       $(this)[0].action="";
     }
   else {
-
+    console.log(submitURL);
     $(this)[0].action=submitURL;
     // on cache la forme apres l'envoie des donnees
     $('#input-form ').hide();
@@ -60,49 +60,60 @@ $('.gift-link').click(function(){
 
  /*  managing the mini-gift system
  if specific item from the gift list has been chosen so it is removed from the gift array*/
- //Liste des cadeaux disponibles
+
+//Liste des cadeaux disponibles
 var giftList_available=[ "Réfrigérateur","Four à gaz ","Micro onde", "Blender ","Ventilateur","TV ","Radio ","Fer à repasser ", "Machine à laver", "Cafetière ", "Toaster ", "Water  cooler", "Tasses ", "Verres", "Couverts", "Assietes ", "Rideaux ",   "Jeu de couteaux", "Draps", "Coffee maker", "Lampe de nuit"];
 giftList_available.sort();
 //Liste des cadeaux selectionnés
-var giftList_Taken= [];
+ var giftList_Taken= [];
 
 //function that retrieve the list of seelected gift from the spreadsheet on googledrive using the google api explorer
-$.getJSON('https://sheets.googleapis.com/v4/spreadsheets/1EW0v6Retxj9_LFraUxQDjY6-UdvhWIxBLUQbOHZOuNo/values/g2%3Ag120?key=AIzaSyBvyJ-3XmqfZhThVnYU68nR32pnVyf0Ao0' , function(data) {
-         //Si la liste est vide on affiche l'ensemble des cadeaux
-        if(data.values.length ===0){
-          $.each(giftList_available, function(index, element){
-             $('.gift-select').append("<option id='input-gift' value='"+ element +"'>"+ element +"</option>");
+$.getJSON('https://sheets.googleapis.com/v4/spreadsheets/1EW0v6Retxj9_LFraUxQDjY6-UdvhWIxBLUQbOHZOuNo/values/g2%3Ag69?key=AIzaSyBvyJ-3XmqfZhThVnYU68nR32pnVyf0Ao0' , function(data) {
+            //Si la liste est vide on affiche l'ensemble des cadeaux
+           if(data.values.length === 0){
+             $.each(giftList_available, function(index, element){
+                $('.gift-select').append("<option value='"+ element +"'>"+ element +"</option>");
 
-            //affichage des cadeaux avec distinction dispo ou non dispo basee sur le style
-            $('.list-type2').append("<li><a href='#'>"+ element+"</a></li>");
-          });
+               //affichage des cadeaux avec distinction dispo ou non dispo basee sur le style
+               $('.list-type2').append("<li><a href='#'>"+ element+"</a></li>");
+             });
 
-          //sinon on affichera l'ensemble des cadeaux non selectionnés en affichage
-        }else{
-          $.each(data.values, function(index, element) {
-           giftList_Taken.push(element);
-           giftList_available = giftList_available.filter(function(i) {
-             return i != element
-           });
-           $.each(giftList_available,function(index,element1){
-             $('.gift-select').append("<option id='input-gift' value='"+ element1 +"'>"+ element1 +"</option>");
-             $('.list-type2').append("<li><a href='#'>"+ element1+"</a></li>");
-          });
+             //sinon on affichera l'ensemble des cadeaux non selectionnés en affichage
+           }else{
+             //console.log(data.values);
+             $.each(data.values, function(index, element) {
+               //console.log(data.values[index][0]);
+                 //console.log( element[0]);
+                 giftList_available = giftList_available.filter(function(i) {
+                if(i == element[0]){
+                  giftList_Taken.push(element[0]);
+                }
+                return i != element[0]
 
-            //alert(giftList_Taken);
-    });
-           //on ajoute les elements deja choisis a la liste de cadeaux
-          $.each(giftList_Taken,function(index,element2){
-              $('.list-type2').append("<li class='gift_taken'><a href='#'>"+ element2+"</a></li>");
-          });
-        }
-           //on ajoute autres en fin de liste pour le dropdown.
-         $('.gift-select').append("<option value='Autres...'>Autres...</option>");
-        })
-.done(function() {
-   $('.list-type2').append("<li ><a href='#'>Autres... </a></li>");
-            })
-.fail(function() {  $('.list-type2').text("Erreur lors du chargement de donnée"); });
+              });
+               console.log(giftList_available);
+               console.log(giftList_Taken);
+
+
+       });
+             $.each(giftList_available,function(index,element1){
+                $('.gift-select').append("<option value='"+ element1 +"'>"+ element1 +"</option>");
+                $('.list-type2').append("<li><a href='#'>"+ element1+"</a></li>");
+             });
+              //on ajoute les elements deja choisis a la liste de cadeaux
+             $.each(giftList_Taken,function(index,element2){
+                 $('.list-type2').append("<li class='gift_taken'><a href='#'>"+ element2+"</a></li>");
+             });
+           }
+              //on ajoute autres en fin de liste pour le dropdown.
+            $('.gift-select').append("<option value='Autres...'>Autres...</option>");
+
+
+           })
+  .done(function() {
+      $('.list-type2').append("<li ><a href='#'>Autres... </a></li>");
+               })
+  .fail(function() {  $('.list-type2').text("Erreur lors du chargement de donnée"); });
 
          /*
 //link to see results in html table
